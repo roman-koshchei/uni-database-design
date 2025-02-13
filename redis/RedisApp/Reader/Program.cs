c9822965e1623804cdf2324f83a7a52bd7f43b2a
Roman Koshchei <romankoshchei@gmail.com>
2025-02-13 20:20:44 +0200

    redis full

---
+++ redis/RedisApp/Reader/Program.cs
@@ -0,0 +1,48 @@
+﻿using StackExchange.Redis;
+
+Console.WriteLine("Reader!");
+
+var cancellation = new CancellationTokenSource();
+
+Console.CancelKeyPress += (sender, e) =>
+{
+    e.Cancel = true;
+    cancellation.Cancel();
+    Console.WriteLine("Ctrl+C pressed. Exiting gracefully...");
+};
+
+var connectionString = "localhost:6379";
+ConnectionMultiplexer redis = ConnectionMultiplexer.Connect(connectionString);
+IDatabase db = redis.GetDatabase();
+
+string stream = "card";
+
+string? lastId = "0";
+try
+{
+    while (!cancellation.Token.IsCancellationRequested)
+    {
+        var entries = db.StreamRead(stream, lastId, 10);
+
+        Console.WriteLine("Shopping cart contents:");
+        foreach (var entry in entries)
+        {
+            lastId = entry.Id;
+            foreach (var field in entry.Values)
+            {
+                Console.WriteLine($"{field.Name}: {field.Value}");
+            }
+        }
+
+        await Task.Delay(5000);
+    }
+}
+catch (OperationCanceledException)
+{
+    Console.WriteLine("Reader operation canceled.");
+}
+finally
+{
+    cancellation.Dispose();
+    Console.WriteLine("Reader application terminated.");
+}
---
+++ redis/RedisApp/Writer/Program.cs
@@ -0,0 +1,46 @@
+﻿using StackExchange.Redis;
+
+Console.WriteLine("Writer!");
+
+var cancellation = new CancellationTokenSource();
+
+Console.CancelKeyPress += (sender, e) =>
+{
+    e.Cancel = true;
+    cancellation.Cancel();
+    Console.WriteLine("Ctrl+C pressed. Exiting gracefully...");
+};
+
+var connectionString = "localhost:6379";
+ConnectionMultiplexer redis = ConnectionMultiplexer.Connect(connectionString);
+IDatabase db = redis.GetDatabase();
+
+string stream = "card";
+
+string user = "user123";
+int counter = 1;
+
+try
+{
+    while (!cancellation.Token.IsCancellationRequested)
+    {
+        string item = $"item {counter}";
+
+        await db.StreamAddAsync(stream, [new("user", user), new("item", item)]);
+
+        Console.WriteLine($"Writing {item} to card.");
+
+        counter += 1;
+
+        await Task.Delay(2000);
+    }
+}
+catch (OperationCanceledException)
+{
+    Console.WriteLine("Writer operation canceled.");
+}
+finally
+{
+    cancellation.Dispose();
+    Console.WriteLine("Writer application terminated.");
+}
